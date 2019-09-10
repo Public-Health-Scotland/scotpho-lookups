9b91d8cb52c4cff754061b5be61dd83085ab0892
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-09-10 15:16:13 +0100

    simd data from open data platform

--- geography_lookup.R
+++ geography_lookup.R
@@ -33,48 +33,58 @@ if (sessionInfo()$platform %in% c("x86_64-redhat-linux-gnu (64-bit)", "x86_64-pc
 ###############################################.
 ## Functions ----
 ###############################################.
+# Function to extract data from the open data platform. The resource_id is
+# static id used in the platform
+extract_data <- function(resource_id) {
+  open_data_url <- "https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id="
+  
+  # Limit is set to 1 million, check nothing is bigger than that
+  data_extraction <-  GET(paste0(open_data_url, resource_id, "&limit=1000000"))
+  
+  # It's a JSON file, so transforming into something more usable in R
+  data_extraction <- rawToChar(data_extraction$content) %>% fromJSON()
+  data_extraction <- data_extraction$result$records %>% #extracting data  
+    setNames(tolower(names(.)))   #variables to lower case
+}
+
+###############################################.
 #Function to create deprivation rank data set for different years 
 #and different simd versions
-create_simd <- function(file_name, simd_version, year, list_pos) {
+create_simd <- function(resource_id, year_list, simd_version, dz) { #list_pos
   
-  #Creating quintile variables to keep
-  simd_sc <- paste0("simd", simd_version, "_sc_quintile")
-  simd_hb <- paste0("simd", simd_version, "_hb2014_quintile")
-  simd_ca <-   if (file_name == "DataZone2001") {
-    paste0("simd", simd_version, "_ca_quintile")
-  } else if (file_name == "DataZone2011") {
-    paste0("simd", simd_version, "_ca2011_quintile")
-  }
+  # Creating quintile variables to keep, they vary with the version
+  sc_quin_var <- paste0("simd", simd_version, "countryquintile")
+  hb_quin_var <- paste0("simd", simd_version, "hb2014quintile")
+  ca_quin_var <- paste0("simd", simd_version, "ca2011quintile")
   
-  if (file_name == "DataZone2001") {
-    data_simd <- readRDS(paste0(cl_out_depr, 'DataZone2001_all_simd.rds')) 
-  } else if (file_name == "DataZone2011") {
-    data_simd <- readRDS(paste0(cl_out_depr, 'DataZone2011_simd2016.rds'))
-  }
-  
-  data_simd <- data_simd %>% setNames(tolower(names(.))) %>%    #variables to lower case
-    select(eval(tolower(file_name)), hb2019, ca2019, eval(simd_sc), eval(simd_hb), eval(simd_ca)) %>% 
-    rename_(datazone = tolower(file_name), sc_quin = eval(simd_sc), 
-            hb_quin = eval(simd_hb), ca_quin = eval(simd_ca)) %>% 
-    mutate(year = year) 
+  # Extract the data, keep some variables and rename them
+  data_simd <- extract_data(resource_id = resource_id) %>% 
+    select({{dz}}, ca2011, hb2014, sc_quin_var, hb_quin_var, ca_quin_var ) %>% 
+    rename(datazone = {{dz}}, ca = ca2011, hb = hb2014, sc_quin = sc_quin_var,
+           hb_quin = hb_quin_var, ca_quin = ca_quin_var)
   
   # recode simd 2004 and 2006, as they follow an inverse scale.
   if (simd_version %in% c(2004, 2006)) {
     data_simd <- data_simd %>%
       mutate_at(vars(sc_quin, ca_quin, hb_quin),
-                funs(recode(., "1" = 5, "2" = 4, "4" = 2, "5" = 1)))
+                ~recode(., "1" = 5, "2" = 4, "3" = 3, "4" = 2, "5" = 1))
   } else {
     data_simd <- data_simd
   }
   
-  data_depr_simd[[list_pos]] <<- data_simd #assigning to list
-  
+  # Creating a list of datasets with all the years used for each version
+  data_simd_list <- lapply(year_list,
+                           function(year_chosen){
+                             data_simd %>% mutate(year = year_chosen)
+                           } )
+  # Joining all the dataframes in one
+  data_simd <- do.call("rbind", data_simd_list) 
 }
 
 ###############################################.
 # Function to create dictionaries for the different areas
 create_dictionary <- function(area_name, area_code, filename) {
-  dictio <- data_extraction %>% 
+  dictio <- dz_base %>% 
     select({{area_name}}, {{area_code}}) %>% 
     rename(areaname = {{area_name}}, code = {{area_code}}) %>% unique
   
@@ -147,32 +157,21 @@ saveRDS(adp_lookup, paste0(geo_lookup, 'ADP_CA_lookup.rds'))
 ###############################################.
 # reading datazone lookup  from open data platform 
 # which includes most geographies but adp and locality
-# URL used for data extraction
-open_data_url <- "https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id="
-
-# Limit is set to 1 million, check nothing is bigger than that
-data_extraction <-  GET(paste0(open_data_url, "395476ab-0720-4740-be07-ff4467141352", 
-                               "&limit=1000000"))
-
-# It's a JSON file, so transforming into something more usable in R
-data_extraction <- rawToChar(data_extraction$content) %>% fromJSON()
-data_extraction <- data_extraction$result$records %>% #extracting data  
-  setNames(tolower(names(.)))   #variables to lower case
+dz_base <- extract_data("395476ab-0720-4740-be07-ff4467141352")
 
 # Creating dz lookup
-dz11_lookup <- data_extraction %>% 
+dz11_lookup <- dz_base %>% 
   select(dz2011, iz2011, ca2011, hscp2016, hb2014) %>% 
   # the variables contain a year, but they have the latest(2019) codes
   rename(datazone2011 = dz2011, intzone2011 =iz2011, ca2019 = ca2011,
          hscp2019 = hscp2016, hb2019 = hb2014)
 
 # merging localities
-dz11_lookup <- left_join(dz11_lookup, hscp_loc, c("datazone2011")) %>% 
-  select(-hscplocality, -hscp2019name)
+dz11_lookup <- left_join(dz11_lookup, hscp_loc, c("datazone2011")) 
 
 # merging adps
 dz11_lookup <- left_join(dz11_lookup, adp_lookup, c("ca2019")) %>% 
-  select(-adp_name)
+  select(-hscplocality, -hscp2019name, -adp_name)
 
 saveRDS(dz11_lookup, paste0(geo_lookup, 'DataZone11_All_Geographies_Lookup.rds'))
 
@@ -216,22 +215,19 @@ saveRDS(code_dictio, paste0(geo_lookup, 'codedictionary.rds'))
 ###############################################.
 ## Part 6 -Deprivation (SIMD) geographies ----
 ###############################################.
-# Creating a data frame with all ranks for each year
-data_depr_simd <- list() #creating empty list for placing data created by function
-
-# The function creates the dataset and assigns it to the list
-mapply(create_simd, file_name = "DataZone2001", simd_version = 2004, 
-       year = 1996:2003, list_pos = 1:8) #simd version 2004
-mapply(create_simd, file_name = "DataZone2001", simd_version = 2006, 
-       year = 2004:2006, list_pos = 9:11) #simd version 2006
-mapply(create_simd, file_name = "DataZone2001", simd_version = "2009v2", 
-       year = 2007:2009, list_pos = 12:14) #simd version 2009
-mapply(create_simd, file_name = "DataZone2001", simd_version = 2012, 
-       year = 2010:2013, list_pos = 15:18) #simd version 2012
-mapply(create_simd, file_name = "DataZone2011", simd_version = 2016, 
-       year = 2014:2018, list_pos = 19:23) #simd version 2016
-
-data_depr_simd <- do.call("rbind", data_depr_simd) # converting from list into dataframe
+# Joining together the extractions of each simd version. 
+data_depr_simd <- rbind(
+  create_simd("a97fca71-ebbb-4897-a611-88024a76ff21", year_list = 1996:2003, 
+              simd_version = 2004, dz = dz2001), #simd version 2004
+  create_simd("6f871d03-d2af-4fe2-a615-d2d2ca76c3a5", year_list = 2004:2006, 
+              simd_version = 2006, dz = dz2001), #simd version 2006
+  create_simd("d9738550-4cf9-428e-8453-c2aad463ff68", year_list = 2007:2009, 
+              simd_version = 2009, dz = dz2001), #simd version 2009
+  create_simd("dd4b13d3-066b-4714-bb1f-730e1a1ee692", year_list = 2010:2013, 
+              simd_version = 2012, dz = dz2001), #simd version 2012
+  create_simd("cadf715a-c365-4dcf-a6e0-acd7e3af21ec", year_list = 2014:2018, 
+              simd_version = 2016, dz = dz2011) #simd version 2016
+)
 
 saveRDS(data_depr_simd, paste0(geo_lookup, 'deprivation_geography.rds'))
 

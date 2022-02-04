0b27a980a6476a2629be6606c3d74fd5f53a00b5
monicamcgibbon <monica.mcgibbon@phs.scot>
2022-02-04 15:47:56 +0000

    Updating live births population lookup to include 2020

--- population_lookup.R
+++ population_lookup.R
@@ -362,7 +362,7 @@ saveRDS(teenpreg_pop_depr, file=paste0(pop_lookup, 'depr_pop_fem15to19.rds'))
 ###############################################.
 # Live births (used for infant deaths under 1)
 live_births <- readxl::read_excel(paste0("/PHI_conf/ScotPHO/Profiles/Data/Received Data/",
-                                 "Births 2002-2019 datazone_2011.xlsx")) %>%
+                                 "Births 2002-2020 datazone_2011.xlsx")) %>%
   janitor::clean_names() %>% 
   rename(datazone = datazone_2011, year = registration_year) %>% 
   group_by(year, datazone) %>% 
